4b9d844df18058e5724ea96e0e56d378862625b6
Christian Gudrian <cgu@ivu.de>
2024-01-08 19:32:21 +0100

    added .clang-format and re-formatted code

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,12 @@
 
 #include <InfluxDbClient.h>
 
-#include "WiFiManager.h"
-#include "webServer.h"
-#include "updater.h"
 #include "configManager.h"
 #include "dashboard.h"
+#include "updater.h"
+#include "webServer.h"
+#include "WiFiManager.h"
+
 
 #define TZ_INFO "CET-1CEST,M3.5.0,M10.5.0/3"
 
@@ -22,39 +23,38 @@ WiFiServer Dumper(4711);
 
 void applyConfiguration()
 {
-  Influx.setConnectionParams(
-      configManager.data.influxdbUrl,
-      configManager.data.influxdbOrg,
-      configManager.data.influxdbBucket,
-      configManager.data.influxdbToken);
+    Influx.setConnectionParams(configManager.data.influxdbUrl,
+                               configManager.data.influxdbOrg,
+                               configManager.data.influxdbBucket,
+                               configManager.data.influxdbToken);
 }
 
 void setup()
 {
-  pinMode(LED_BUILTIN_AUX, OUTPUT);
-  digitalWrite(LED_BUILTIN_AUX, HIGH);
+    pinMode(LED_BUILTIN_AUX, OUTPUT);
+    digitalWrite(LED_BUILTIN_AUX, HIGH);
 
-  pinMode(RS485_DE, OUTPUT);
-  digitalWrite(RS485_DE, LOW);
+    pinMode(RS485_DE, OUTPUT);
+    digitalWrite(RS485_DE, LOW);
 
-  Serial.begin(115200);
-  RS485.begin(9600, SWSERIAL_8S1, RS485_RO, RS485_DI, false);
+    Serial.begin(115200);
+    RS485.begin(9600, SWSERIAL_8S1, RS485_RO, RS485_DI, false);
 
-  LittleFS.begin();
-  GUI.begin();
-  configManager.begin();
-  dash.begin();
-  WiFiManager.begin(configManager.data.projectName);
+    LittleFS.begin();
+    GUI.begin();
+    configManager.begin();
+    dash.begin();
+    WiFiManager.begin(configManager.data.projectName);
 
-  applyConfiguration();
-  configManager.setConfigSaveCallback(applyConfiguration);
+    applyConfiguration();
+    configManager.setConfigSaveCallback(applyConfiguration);
 
-  Dumper.begin();
+    Dumper.begin();
 
-  timeSync(TZ_INFO, "de.pool.ntp.org", "pool.ntp.org");
+    timeSync(TZ_INFO, "de.pool.ntp.org", "pool.ntp.org");
 
-  digitalWrite(LED_BUILTIN_AUX, LOW);
-  Serial.println("Ready");
+    digitalWrite(LED_BUILTIN_AUX, LOW);
+    Serial.println("Ready");
 }
 
 size_t packetSize;
@@ -62,112 +62,103 @@ uint8_t buffer[128];
 unsigned long lastByteTime;
 bool readingPacket;
 
-void influxWrite(Point &p)
+void influxWrite(Point& p)
 {
-  if (!Influx.writePoint(p))
-  {
-    Serial.print("InfluxDB Client error: ");
-    Serial.println(Influx.getLastErrorMessage());
-  }
+    if (!Influx.writePoint(p)) {
+        Serial.print("InfluxDB Client error: ");
+        Serial.println(Influx.getLastErrorMessage());
+    }
 }
 
 WiFiClient dumperClient;
 
 void processPacket()
 {
-  if (packetSize < 2)
-    return;
+    if (packetSize < 2)
+        return;
 
-  if (dumperClient) {
-    dumperClient.printf("[%02x]", buffer[0]);
-    for (size_t i = 1; i < packetSize; ++i)
-      dumperClient.printf(" %02x", buffer[i]);
-    dumperClient.print('\n');
-  }
+    if (dumperClient) {
+        dumperClient.printf("[%02x]", buffer[0]);
+        for (size_t i = 1; i < packetSize; ++i)
+            dumperClient.printf(" %02x", buffer[i]);
+        dumperClient.print('\n');
+    }
 
-  {
-    Point packet("packet");
-    packet.addTag("address", String(buffer[0], 16));
-    packet.addTag("lastByte", String(buffer[packetSize - 1], 16));
-    packet.addField("size", packetSize);
-    influxWrite(packet);
-  }
-
-  if (buffer[0] == 0x41 && packetSize == 30)
-  {
-    Point raw("raw");
-    raw.addTag("index", String(buffer[18]));
-    raw.addField("val0", buffer[19]);
-    raw.addField("val1", buffer[20]);
-    raw.addField("val2", buffer[21]);
-    raw.addField("val3", buffer[22]);
-    raw.addField("val4", buffer[23]);
-    raw.addField("val5", buffer[24]);
-    raw.addField("val6", buffer[25]);
-    raw.addField("val7", buffer[26]);
-    influxWrite(raw);
-
-    switch (buffer[18])
     {
-    case 0x3: {
-      Point temperatures("temperatures");
-      temperatures.addField("vorlauf", buffer[19]);
-      temperatures.addField("rücklauf", buffer[20]);
-      temperatures.addField("warmwasser", buffer[21]);
-      temperatures.addField("außen", static_cast<int8_t>(buffer[22]));
-      temperatures.addField("abgas", buffer[23]);
-      influxWrite(temperatures);
-      break;
+        Point packet("packet");
+        packet.addTag("address", String(buffer[0], 16));
+        packet.addTag("lastByte", String(buffer[packetSize - 1], 16));
+        packet.addField("size", packetSize);
+        influxWrite(packet);
     }
 
-    case 0x6: {
-      Point pressures("pressures");
-      pressures.addField("anlage", buffer[26] / 10.0f, 1);
-      influxWrite(pressures);
-      break;
-    }
+    if (buffer[0] == 0x41 && packetSize == 30) {
+        Point raw("raw");
+        raw.addTag("index", String(buffer[18]));
+        raw.addField("val0", buffer[19]);
+        raw.addField("val1", buffer[20]);
+        raw.addField("val2", buffer[21]);
+        raw.addField("val3", buffer[22]);
+        raw.addField("val4", buffer[23]);
+        raw.addField("val5", buffer[24]);
+        raw.addField("val6", buffer[25]);
+        raw.addField("val7", buffer[26]);
+        influxWrite(raw);
 
+        switch (buffer[18]) {
+            case 0x3: {
+                Point temperatures("temperatures");
+                temperatures.addField("vorlauf", buffer[19]);
+                temperatures.addField("rücklauf", buffer[20]);
+                temperatures.addField("warmwasser", buffer[21]);
+                temperatures.addField("außen", static_cast<int8_t>(buffer[22]));
+                temperatures.addField("abgas", buffer[23]);
+                influxWrite(temperatures);
+                break;
+            }
+
+            case 0x6: {
+                Point pressures("pressures");
+                pressures.addField("anlage", buffer[26] / 10.0f, 1);
+                influxWrite(pressures);
+                break;
+            }
+        }
+    } else if (buffer[0] == 0x71 && packetSize == 41) {
     }
-  }
-  else if (buffer[0] == 0x71 && packetSize == 41)
-  {
-  }
 }
 
 void loop()
 {
-  WiFiManager.loop();
-  updater.loop();
-  configManager.loop();
-  dash.loop();
+    WiFiManager.loop();
+    updater.loop();
+    configManager.loop();
+    dash.loop();
 
-  if (auto newDumperClient = Dumper.available())
-    dumperClient = newDumperClient;
+    if (auto newDumperClient = Dumper.available())
+        dumperClient = newDumperClient;
 
-  while (RS485.available())
-  {
-    auto b = RS485.read();
-    lastByteTime = millis();
+    while (RS485.available()) {
+        auto b = RS485.read();
+        lastByteTime = millis();
 
-    // address byte starts new packet
-    if (RS485.readParity())
-    {
-      if (readingPacket)
-        processPacket();
-      readingPacket = true;
-      packetSize = 0;
+        // address byte starts new packet
+        if (RS485.readParity()) {
+            if (readingPacket)
+                processPacket();
+            readingPacket = true;
+            packetSize = 0;
+        }
+
+        // add current byte to buffer
+        if (readingPacket && packetSize < sizeof(buffer))
+            buffer[packetSize++] = b;
     }
 
-    // add current byte to buffer
-    if (readingPacket && packetSize < sizeof(buffer))
-      buffer[packetSize++] = b;
-  }
-
-  // process buffer after timeout
-  if (readingPacket && (millis() - lastByteTime > configManager.data.packetTimeout))
-  {
-    processPacket();
-    packetSize = 0;
-    readingPacket = false;
-  }
+    // process buffer after timeout
+    if (readingPacket && (millis() - lastByteTime > configManager.data.packetTimeout)) {
+        processPacket();
+        packetSize = 0;
+        readingPacket = false;
+    }
 }
