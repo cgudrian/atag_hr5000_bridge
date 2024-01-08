b157ecb158198779b7ba9474e249a73602245b33
Christian Gudrian <cgu@ivu.de>
2024-01-09 00:07:41 +0100

    added basic MQTT communication

--- src/main.cpp
+++ src/main.cpp
@@ -20,16 +20,69 @@ static constexpr int RS485_RO = 14;
 static SoftwareSerial RS485;
 static InfluxDBClient Influx;
 static WiFiServer Dumper(4711);
+static MQTTClient mqtt;
+static WiFiClient net;
+
+static const auto& cfg = configManager.data;
+static const String MQTT_PREFIX = String("atagbridge/");
+static String onlineTopic;
+static String heartbeatTopic;
+
+static void updateMqttTopics()
+{
+    auto p = MQTT_PREFIX + cfg.hostname + "/";
+    onlineTopic = p + "online";
+    heartbeatTopic = p + "heartbeat";
+}
+
+static void mqttConnect()
+{
+    Serial.print("Connecting to MQTT broker...");
+    if (mqtt.connect(cfg.hostname)) {
+        Serial.println("OK.");
+        mqtt.setWill(onlineTopic.c_str(), "0", true, 1);
+        mqtt.publish(onlineTopic.c_str(), "1", true, 1);
+    } else {
+        Serial.println("FAILED.");
+    }
+}
+
+static void mqttDisconnect()
+{
+    mqtt.publish(onlineTopic.c_str(), "0", true, 1);
+    mqtt.clearWill();
+    mqtt.disconnect();
+}
 
 static void applyConfiguration()
 {
-    Influx.setConnectionParams(configManager.data.influxdbUrl,
-                               configManager.data.influxdbOrg,
-                               configManager.data.influxdbBucket,
-                               configManager.data.influxdbToken);
+    static String lastHostname;
+    static String lastMqttHost;
+    static int lastMqttPort;
 
-    if (strlen(configManager.data.hostname))
-        WiFi.setHostname(configManager.data.hostname);
+    Serial.print("Applying Configuration...");
+
+    if (lastHostname != cfg.hostname) {
+        Serial.print("Hostname...");
+        mqttDisconnect();
+        WiFi.setHostname(cfg.hostname);
+        updateMqttTopics();
+    }
+
+    Serial.print("InfluxDB...");
+    Influx.setConnectionParams(cfg.influxdbUrl, cfg.influxdbOrg, cfg.influxdbBucket, cfg.influxdbToken);
+
+    if (lastMqttHost != cfg.mqttBrokerHost || lastMqttPort != cfg.mqttBrokerPort || lastHostname != cfg.hostname) {
+        Serial.print("MQTT...");
+        mqttDisconnect();
+        mqtt.setHost(cfg.mqttBrokerHost, cfg.mqttBrokerPort);
+    }
+
+    lastHostname = cfg.hostname;
+    lastMqttHost = cfg.mqttBrokerHost;
+    lastMqttPort = cfg.mqttBrokerPort;
+
+    Serial.println("Done.");
 }
 
 void setup()
@@ -41,25 +94,40 @@ void setup()
     digitalWrite(RS485_DE, LOW);
 
     Serial.begin(115200);
+    Serial.println("Booting...");
+
     RS485.begin(9600, SWSERIAL_8S1, RS485_RO, RS485_DI, false);
 
-    LittleFS.begin();
-    GUI.begin();
     configManager.begin();
-    dash.begin();
 
-    WiFiManager.begin(configManager.data.projectName);
-    WiFi.setAutoConnect(true);
+    Serial.println("Current Configuration:");
+    Serial.println("----------------------");
+    Serial.print("Hostname: ");
+    Serial.println(cfg.hostname);
+    Serial.print("MQTT Broker: ");
+    Serial.print(cfg.mqttBrokerHost);
+    Serial.print(":");
+    Serial.println(cfg.mqttBrokerPort);
 
     applyConfiguration();
     configManager.setConfigSaveCallback(applyConfiguration);
+
+    WiFiManager.begin(cfg.projectName);
+    WiFi.setAutoReconnect(true);
+
+    LittleFS.begin();
+    GUI.begin();
+    dash.begin();
 
     Dumper.begin();
 
     timeSync(TZ_INFO, "de.pool.ntp.org", "pool.ntp.org");
 
+    mqtt.begin(cfg.mqttBrokerHost, cfg.mqttBrokerPort, net);
+    mqttConnect();
+
     digitalWrite(LED_BUILTIN_AUX, LOW);
-    Serial.println("Ready");
+    Serial.println("Ready.");
 }
 
 static void influxWrite(Point& p)
@@ -135,10 +203,28 @@ static void processPacket()
 
 void loop()
 {
+    static constexpr auto MQTT_RECONNECT_INTERVAL = 1000;
+    static constexpr auto HEARTBEAT_INTERVAL = 1000;
+
+    static auto nextMqttConnectTime = millis() + MQTT_RECONNECT_INTERVAL;
+    static auto nextHeartbeatTime = millis() + HEARTBEAT_INTERVAL;
+    static uint32_t heartbeatCounter;
+
     WiFiManager.loop();
     updater.loop();
     configManager.loop();
     dash.loop();
+    mqtt.loop();
+
+    if (!mqtt.connected() && millis() >= nextMqttConnectTime) {
+        mqttConnect();
+        nextMqttConnectTime = millis() + MQTT_RECONNECT_INTERVAL;
+    }
+
+    if (mqtt.connected() && millis() >= nextHeartbeatTime) {
+        mqtt.publish(heartbeatTopic.c_str(), std::to_string(heartbeatCounter++).c_str());
+        nextHeartbeatTime += HEARTBEAT_INTERVAL;
+    }
 
     if (auto newDumperClient = Dumper.accept())
         dumperClient = std::move(newDumperClient);
@@ -161,7 +247,7 @@ void loop()
     }
 
     // process buffer after timeout
-    if (readingPacket && (millis() - lastByteTime > configManager.data.packetTimeout)) {
+    if (readingPacket && (millis() - lastByteTime > cfg.packetTimeout)) {
         processPacket();
         packetSize = 0;
         readingPacket = false;
