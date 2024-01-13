dd5734424636577c26db771b77e70d852ab32a28
Christian Gudrian <cgu@ivu.de>
2024-01-13 22:54:21 +0100

    began transitioning to MQTT

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,20 @@
 #include <webServer.h>
 #include <WiFiManager.h>
 
+#include <map>
+
 // TODO: turn into configuration option
 #define TZ_INFO "CET-1CEST,M3.5.0,M10.5.0/3"
 
-static constexpr int RS485_DE = 12;
-static constexpr int RS485_DI = 13;
-static constexpr int RS485_RO = 14;
+#ifndef MQTT_PREFIX
+#define MQTT_PREFIX "atagbridge"
+#endif
+
+static auto const MAX_POWER = 21.6f;
+
+static int const RS485_DE = 12;
+static int const RS485_DI = 13;
+static int const RS485_RO = 14;
 
 static SoftwareSerial RS485;
 static InfluxDBClient Influx;
@@ -23,25 +31,32 @@ static WiFiServer Dumper(4711);
 static MQTTClient mqtt;
 static WiFiClient net;
 
-static const auto& cfg = configManager.data;
-static const String MQTT_PREFIX = String("atagbridge/");
-static String onlineTopic;
-static String heartbeatTopic;
+static auto const& cfg = configManager.data;
 
-static void updateMqttTopics()
-{
-    auto p = MQTT_PREFIX + cfg.hostname + "/";
-    onlineTopic = p + "online";
-    heartbeatTopic = p + "heartbeat";
-}
+namespace topics {
+static char const* const online = MQTT_PREFIX "/online";
+static char const* const flowTemperature = MQTT_PREFIX "/temperature/flow";
+static char const* const returnTemperature = MQTT_PREFIX "/temperature/return";
+static char const* const differenceTemperature = MQTT_PREFIX "/temperature/difference";
+static char const* const hotWaterTemperature = MQTT_PREFIX "/temperature/hotwater";
+static char const* const outsideTemperature = MQTT_PREFIX "/temperature/outside";
+static char const* const exhaustTemperature = MQTT_PREFIX "/temperature/exhaust";
+static char const* const nominalTemperature = MQTT_PREFIX "/temperature/nominal";
+static char const* const pressure = MQTT_PREFIX "/pressure";
+static char const* const power = MQTT_PREFIX "/power";
+static char const* const energy = MQTT_PREFIX "/energy";
+static char const* const state = MQTT_PREFIX "/state";
+static char const* const pump = MQTT_PREFIX "/toggles/pump";
+static char const* const heating = MQTT_PREFIX "/toggles/heating";
+static char const* const hotwater = MQTT_PREFIX "/toggles/hotwater";
+}  // namespace topics
 
 static void mqttConnect()
 {
     Serial.print("Connecting to MQTT broker...");
     if (mqtt.connect(cfg.hostname)) {
         Serial.println("OK.");
-        mqtt.setWill(onlineTopic.c_str(), "0", true, 1);
-        mqtt.publish(onlineTopic.c_str(), "1", true, 1);
+        mqtt.publish(topics::online, "1", true, 1);
     } else {
         Serial.println("FAILED.");
     }
@@ -49,7 +64,7 @@ static void mqttConnect()
 
 static void mqttDisconnect()
 {
-    mqtt.publish(onlineTopic.c_str(), "0", true, 1);
+    mqtt.publish(topics::online, "0", true, 1);
     mqtt.clearWill();
     mqtt.disconnect();
 }
@@ -62,21 +77,21 @@ static void applyConfiguration()
 
     Serial.print("Applying Configuration...");
 
-    if (lastHostname != cfg.hostname) {
-        Serial.print("Hostname...");
-        mqttDisconnect();
-        WiFi.setHostname(cfg.hostname);
-        updateMqttTopics();
-    }
-
-    Serial.print("InfluxDB...");
-    Influx.setConnectionParams(cfg.influxdbUrl, cfg.influxdbOrg, cfg.influxdbBucket, cfg.influxdbToken);
-
-    if (lastMqttHost != cfg.mqttBrokerHost || lastMqttPort != cfg.mqttBrokerPort || lastHostname != cfg.hostname) {
+    if (lastMqttHost != cfg.mqttBrokerHost || lastMqttPort != cfg.mqttBrokerPort) {
         Serial.print("MQTT...");
         mqttDisconnect();
         mqtt.setHost(cfg.mqttBrokerHost, cfg.mqttBrokerPort);
     }
+
+    if (lastHostname != cfg.hostname) {
+        Serial.print("Hostname...");
+        mqttDisconnect();
+        mqtt.setWill(topics::online, "0", 1, true);
+        WiFi.setHostname(cfg.hostname);
+    }
+
+    Serial.print("InfluxDB...");
+    Influx.setConnectionParams(cfg.influxdbUrl, cfg.influxdbOrg, cfg.influxdbBucket, cfg.influxdbToken);
 
     lastHostname = cfg.hostname;
     lastMqttHost = cfg.mqttBrokerHost;
@@ -123,7 +138,7 @@ void setup()
 
     timeSync(TZ_INFO, "de.pool.ntp.org", "pool.ntp.org");
 
-    mqtt.begin(cfg.mqttBrokerHost, cfg.mqttBrokerPort, net);
+    mqtt.begin(net);
     mqttConnect();
 
     digitalWrite(LED_BUILTIN_AUX, LOW);
@@ -144,6 +159,35 @@ static size_t packetSize;
 static uint8_t buffer[128];
 static unsigned long lastByteTime;
 static bool readingPacket;
+
+class Integrator {
+  public:
+    float update(float value)
+    {
+        auto now = millis();
+
+        if (!firstTime) {
+            auto deltaT = (now - lastT) / 1000.0;
+            // integration with linear interpolation
+            result += 0.5 * (lastValue + value) * deltaT;
+        } else {
+            firstTime = false;
+        }
+
+        lastValue = value;
+        lastT = now;
+
+        return result;
+    }
+
+  private:
+    bool firstTime{true};
+    decltype(millis()) lastT{};
+    float lastValue{};
+    float result{};
+};
+
+static Integrator powerToEnergy;
 
 static void processPacket()
 {
@@ -166,34 +210,84 @@ static void processPacket()
     }
 
     if (buffer[0] == 0x41 && packetSize == 30) {
+        int8_t* val = reinterpret_cast<int8_t*>(&buffer[19]);
+
         Point raw("raw");
         raw.addTag("index", String(buffer[18]));
-        raw.addField("val0", buffer[19]);
-        raw.addField("val1", buffer[20]);
-        raw.addField("val2", buffer[21]);
-        raw.addField("val3", buffer[22]);
-        raw.addField("val4", buffer[23]);
-        raw.addField("val5", buffer[24]);
-        raw.addField("val6", buffer[25]);
-        raw.addField("val7", buffer[26]);
+        for (int i = 0; i < 8; ++i)
+            raw.addField(String("val") + i, val[i]);
         influxWrite(raw);
 
         switch (buffer[18]) {
             case 0x3: {
                 Point temperatures("temperatures");
-                temperatures.addField("vorlauf", buffer[19]);
-                temperatures.addField("rücklauf", buffer[20]);
-                temperatures.addField("warmwasser", buffer[21]);
-                temperatures.addField("außen", static_cast<int8_t>(buffer[22]));
-                temperatures.addField("abgas", buffer[23]);
+                temperatures.addField("vorlauf", val[0]);
+                temperatures.addField("rücklauf", val[1]);
+                temperatures.addField("warmwasser", val[2]);
+                temperatures.addField("außen", static_cast<int8_t>(val[3]));
+                temperatures.addField("abgas", val[4]);
                 influxWrite(temperatures);
+
+                mqtt.publish(topics::flowTemperature, String(val[0]));
+                mqtt.publish(topics::returnTemperature, String(val[1]));
+                mqtt.publish(topics::differenceTemperature, String(val[0] - val[1]));
+                mqtt.publish(topics::hotWaterTemperature, String(val[2]));
+                mqtt.publish(topics::outsideTemperature, String(val[3]));
+                mqtt.publish(topics::exhaustTemperature, String(val[4]));
+                mqtt.publish(topics::nominalTemperature, String(val[7]));
+
+                break;
+            }
+
+            case 0x4: {
+                static std::map<uint8_t, char const*> VALUE_MAP = {
+                    {0, "AUS"},
+                    {1, "LZ"},
+                    {2, "ZZ"},
+                    {3, "HB"},
+                    {4, "WW"},
+                    {5, "KV"},
+                    {6, "RT"},
+                    {7, "NH"},
+                    {8, "NW"},
+                    {9, "KT"},
+                };
+
+                auto v = val[6];
+                auto s = VALUE_MAP[v];
+                if (s)
+                    mqtt.publish(topics::state, s);
+                else
+                    mqtt.publish(topics::state, String(v));
+
                 break;
             }
 
             case 0x6: {
                 Point pressures("pressures");
-                pressures.addField("anlage", buffer[26] / 10.0f, 1);
+                auto p = val[7] / 10.0f;
+                pressures.addField("anlage", p, 1);
                 influxWrite(pressures);
+
+                mqtt.publish(topics::pressure, String(p));
+
+                break;
+            }
+
+            case 0x7: {
+                // power
+                auto power = MAX_POWER * val[6] / 100.0f;
+                mqtt.publish(topics::power, String(power));
+
+                // integrate and convert to kWh
+                auto energy = powerToEnergy.update(power) / 3600.0f;
+                mqtt.publish(topics::energy, String(energy));
+
+                // operation state
+                auto bits = val[7];
+                mqtt.publish(topics::pump, String((bits & 1) ? 1 : 0));
+                mqtt.publish(topics::heating, String((bits & 2) ? 1 : 0));
+                mqtt.publish(topics::hotwater, String((bits & 4) ? 1 : 0));
                 break;
             }
         }
@@ -203,12 +297,8 @@ static void processPacket()
 
 void loop()
 {
-    static constexpr auto MQTT_RECONNECT_INTERVAL = 1000;
-    static constexpr auto HEARTBEAT_INTERVAL = 1000;
-
+    static auto const MQTT_RECONNECT_INTERVAL = 1000;
     static auto nextMqttConnectTime = millis() + MQTT_RECONNECT_INTERVAL;
-    static auto nextHeartbeatTime = millis() + HEARTBEAT_INTERVAL;
-    static uint32_t heartbeatCounter;
 
     WiFiManager.loop();
     updater.loop();
@@ -219,11 +309,6 @@ void loop()
     if (!mqtt.connected() && millis() >= nextMqttConnectTime) {
         mqttConnect();
         nextMqttConnectTime = millis() + MQTT_RECONNECT_INTERVAL;
-    }
-
-    if (mqtt.connected() && millis() >= nextHeartbeatTime) {
-        mqtt.publish(heartbeatTopic.c_str(), std::to_string(heartbeatCounter++).c_str());
-        nextHeartbeatTime += HEARTBEAT_INTERVAL;
     }
 
     if (auto newDumperClient = Dumper.accept())
